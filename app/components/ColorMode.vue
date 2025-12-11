916d3ad238b6c1c7f9c40cebea22165069dac614
Yu Lia <1938793150@qq.com>
2025-12-11 23:07:24 +0800

    feat: add color mode

---
+++ app/components/ColorMode.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+function switchTheme() {
+  colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+}
+
+function startViewTransition(event: MouseEvent) {
+  if (!document.startViewTransition) {
+    switchTheme()
+    return
+  }
+
+  const x = event.clientX
+  const y = event.clientY
+  const endRadius = Math.hypot(
+    Math.max(x, window.innerWidth - x),
+    Math.max(y, window.innerHeight - y),
+  )
+
+  const transition = document.startViewTransition(() => {
+    switchTheme()
+  })
+
+  transition.ready.then(() => {
+    const duration = 600
+    document.documentElement.animate(
+      {
+        clipPath: [
+          `circle(0px at ${x}px ${y}px)`,
+          `circle(${endRadius}px at ${x}px ${y}px)`,
+        ],
+      },
+      {
+        duration,
+        easing: 'cubic-bezier(.76,.32,.29,.99)',
+        pseudoElement: '::view-transition-new(root)',
+      },
+    )
+  })
+}
+</script>
+
+<template>
+  <ClientOnly>
+    <Button variant="outline" size="icon" aria-label="Toggle theme" @click="startViewTransition">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="size-4.5"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+        <path d="M12 3l0 18" />
+        <path d="M12 9l4.65 -4.65" />
+        <path d="M12 14.3l7.37 -7.37" />
+        <path d="M12 19.6l8.85 -8.85" />
+      </svg>
+    </Button>
+  </ClientOnly>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
     '@nuxt/image',
+    '@nuxtjs/color-mode',
     '@vueuse/nuxt',
     'shadcn-nuxt',
   ],
