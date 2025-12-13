d90ee038c8a3080e2e49b49013ae651feb16a134
Yu Lia <1938793150@qq.com>
2025-12-13 14:14:50 +0800

    chore: configure content

--- content.config.ts
+++ content.config.ts
@@ -1,10 +1,26 @@
-import { defineCollection, defineContentConfig } from '@nuxt/content'
+import { defineCollection, defineContentConfig, z } from '@nuxt/content'
+
+function createPostSchema() {
+  return z.object({
+    title: z.string(),
+    description: z.string(),
+    minRead: z.number(),
+    date: z.date(),
+    hidden: z.boolean().default(false),
+    /**
+     * 优先级，0-1000，1000 为置顶
+     */
+    priority: z.number().min(0).max(1000).int().default(0),
+    tags: z.array(z.string()).default([]),
+  })
+}
 
 export default defineContentConfig({
   collections: {
-    content: defineCollection({
+    posts: defineCollection({
       type: 'page',
-      source: '**',
+      source: 'posts/*.md',
+      schema: createPostSchema(),
     }),
   },
 })
