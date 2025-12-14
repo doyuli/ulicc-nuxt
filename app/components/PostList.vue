faa89b39749b614ee7c7274fa99e0514bd1c2412
Yu Lia <1938793150@qq.com>
2025-12-14 13:27:12 +0800

    feat: layout page

---
+++ app/components/PostList.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import type { PostsCollectionItem } from '@nuxt/content'
+import type { HTMLAttributes } from 'vue'
+import { ArrowUpToLine, CalendarIcon } from 'lucide-vue-next'
+import { cn } from '@/lib/utils'
+import { dateFormatter } from '~/utils/formatter'
+
+const { posts = [] } = defineProps<{
+  posts?: PostsCollectionItem[]
+  class?: HTMLAttributes['class']
+}>()
+</script>
+
+<template>
+  <ItemGroup :class="cn('gap-4', $props.class)">
+    <Item
+      v-for="(post, i) in posts"
+      :key="post.id"
+      variant="outline"
+      as-child
+      role="listitem"
+      class="bg-background animate-fade-up"
+      :style="{ animationDelay: `${0.2 + i * 0.1}s` }"
+    >
+      <NuxtLink :to="post.path">
+        <ItemContent>
+          <ItemTitle>
+            <span>{{ post.title }}</span>
+            <Badge v-if="post.priority! >= 1000" variant="secondary">
+              <ArrowUpToLine class="size-3" />
+            </Badge>
+          </ItemTitle>
+          <ItemDescription>
+            {{ post.description }}
+          </ItemDescription>
+        </ItemContent>
+        <ItemFooter>
+          <div class="flex items-center gap-1 text-xs text-muted-foreground shrink-0">
+            <CalendarIcon class="size-3" />
+            <span>{{ dateFormatter.custom(new Date(post.date), { dateStyle: 'medium' }) }}</span>
+          </div>
+        </ItemFooter>
+      </NuxtLink>
+    </Item>
+  </ItemGroup>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-import { ArrowUpToLine, CalendarIcon } from 'lucide-vue-next'
-import { dateFormatter } from '~/utils/formatter'
-
 const { data: posts } = await useAsyncData('posts-all', () => {
   return queryCollection('posts')
     .order('priority', 'DESC')
@@ -11,54 +8,13 @@ const { data: posts } = await useAsyncData('posts-all', () => {
 </script>
 
 <template>
-  <div class="flex w-full flex-col gap-6">
-    <ItemGroup class="gap-4">
-      <Item
-        v-for="(post, i) in posts"
-        :key="post.id"
-        variant="outline"
-        as-child
-        role="listitem"
-        class="bg-background animate-fade-up"
-        :style="{ animationDelay: `${0.2 + i * 0.1}s` }"
-      >
-        <NuxtLink :to="post.path">
-          <ItemContent>
-            <ItemTitle>
-              <span>{{ post.title }}</span>
-              <Badge v-if="post.priority! >= 1000" variant="secondary">
-                <ArrowUpToLine class="size-3" />
-              </Badge>
-            </ItemTitle>
-            <ItemDescription>
-              {{ post.description }}
-            </ItemDescription>
-          </ItemContent>
-          <ItemFooter>
-            <div class="flex items-center gap-1 text-xs text-muted-foreground shrink-0">
-              <CalendarIcon class="size-3" />
-              <span>{{ dateFormatter.custom(new Date(post.date), { dateStyle: 'medium' }) }}</span>
-            </div>
-          </ItemFooter>
-        </NuxtLink>
-      </Item>
-    </ItemGroup>
+  <div class="py-20 text-center">
+    Banner
+  </div>
+  <div class="flex w-full" style="--sidebar-width: 300px">
+    <div class="w-[calc(100%-var(--sidebar-width))] max-lg:w-full transition-[width] duration-300">
+      <PostList :posts="posts" />
+    </div>
+    <SiteSidebar class="w-(--sidebar-width) max-lg:hidden" />
   </div>
 </template>
-
-<!-- <style>
-@keyframes fade-up {
-  0% {
-    opacity: 0;
-    transform: translateY(20px);
-  }
-  100% {
-    opacity: 1;
-    transform: translateY(0);
-  }
-}
-
-.animate-fade-up {
-  animation: fade-up 0.6s 0.3s backwards;
-}
-</style> -->
