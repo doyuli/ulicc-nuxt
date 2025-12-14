0efe0ec0eb042b3382a10e1c1fe36f12664b3b7b
Yu Lia <1938793150@qq.com>
2025-12-14 15:55:27 +0800

    chore: add author config

--- app/app.config.ts
+++ app/app.config.ts
@@ -7,4 +7,10 @@ export default defineAppConfig({
     title: 'Doyuli',
     description: 'Doyuli\'s Blog',
   },
+  author: {
+    name: '阿沥',
+    description: '分享技术与生活',
+    github: 'https://github.com/doyuli',
+    email: 'doyulicc@gmail.com',
+  },
 })
