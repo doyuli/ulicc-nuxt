e943410e008ecb7b04b1f1f79ab28e29cbeacb22
Yu Lia <1938793150@qq.com>
2025-12-26 15:09:26 +0800

    feat: add random-post

---
+++ app/components/RandomPost.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Dice5Icon } from 'lucide-vue-next'
+import { randomInt } from '~/utils/random'
+import { useConfigProviderContext } from './ConfigProvider.vue'
+
+const { posts } = useConfigProviderContext()
+const router = useRouter()
+
+function goToRandomPost() {
+  if (!posts.value?.length)
+    return
+  const random = posts.value[randomInt(0, posts.value.length - 1)]!
+  router.push(random.path)
+}
+</script>
+
+<template>
+  <Button
+    class="rounded-full"
+    variant="outline"
+    size="icon"
+    aria-label="Random Post"
+    @click="goToRandomPost"
+  >
+    <Dice5Icon class="size-4.5 shrink-0" />
+  </Button>
+</template>
