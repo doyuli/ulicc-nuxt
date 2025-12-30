7d3d9420bd4b0dc8dbafdf61264ed0dbe07902e5
Yu Lia <1938793150@qq.com>
2025-12-30 23:30:54 +0800

    feat: adjust padding in prose-li

--- app/components/content/ProseLi.vue
+++ app/components/content/ProseLi.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <li class="my-1.5 ps-1.5 leading-7.5 [&>ul]:my-0">
+  <li class="my-1.5 ps-0.5 leading-7.5 [&>ul]:my-0">
     <slot />
   </li>
 </template>
