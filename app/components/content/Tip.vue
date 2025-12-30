eec613f9bd49699f853d89ae4c36f9347cd7d67f
Yu Lia <1938793150@qq.com>
2025-12-30 15:09:53 +0800

    feat: add tip component

---
+++ app/components/content/Tip.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { cva } from 'class-variance-authority'
+import { cn } from '~/lib/utils'
+
+defineProps<{
+  type?: 'error' | 'info' | 'success' | 'warning'
+  class?: HTMLAttributes['class']
+}>()
+
+const tipVariants = cva(
+  'my-4 px-5 py-3 bg-card text-card-foreground border border-l-6 shadow-(--card-shadow) rounded-md transition-all leading-relaxed',
+  {
+    variants: {
+      type: {
+        error:
+          'border-l-red-500 dark:border-l-red-400',
+        info:
+          'border-l-primary dark:border-l-primary-light',
+        success:
+         'border-l-emerald-500 dark:border-l-emerald-400',
+        warning:
+          'border-l-amber-500 dark:border-l-amber-400',
+      },
+    },
+    defaultVariants: {
+      type: 'info',
+    },
+  },
+)
+</script>
+
+<template>
+  <div :class="cn(tipVariants({ type }), $props.class)">
+    <slot mdc-unwrap="p" />
+  </div>
+</template>
