e5912853c986bd034e856a6b7f1be22df2f13c14
Yu Lia <1938793150@qq.com>
2026-01-04 15:50:07 +0800

    feat: update marker color in prose

--- app/components/content/ProseOl.vue
+++ app/components/content/ProseOl.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <ol class="list-decimal ps-6 my-5 marker:text-primary-light">
+  <ol class="list-decimal ps-6 my-5 marker:text-secondary-foreground">
     <slot />
   </ol>
 </template>
