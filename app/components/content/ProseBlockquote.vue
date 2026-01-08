3013064c2681da448fd6c35a5216fe124fbb5fa6
Yu Lia <1938793150@qq.com>
2026-01-08 17:30:00 +0800

    fix: typos

---
+++ app/components/content/ProseBlockquote.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <blockquote class="mt-6 my-4 px-4 py-3 bg-card text-card-foreground border border-l-6 rounded-sm leading-7.5 border-l-gray-300 dark:border-l-gray-600">
+    <slot />
+  </blockquote>
+</template>
