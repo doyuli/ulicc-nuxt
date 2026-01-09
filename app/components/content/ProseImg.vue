01afd03a9eaa355003ac3f5805316b936c35ce93
Yu Lia <1938793150@qq.com>
2026-01-09 17:56:58 +0800

    refactor: standardize prose-img

--- app/components/content/ProseImg.vue
+++ app/components/content/ProseImg.vue
@@ -1,5 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ImageComponent from '#build/mdc-image-component.mjs'
+import { joinURL, withLeadingSlash, withTrailingSlash } from 'ufo'
+
+const props = defineProps<{
+  src: string
+  alt: string
+  width?: string | number
+  height?: string | number
+}>()
+
+const refinedSrc = computed(() => {
+  if (props.src?.startsWith('/') && !props.src.startsWith('//')) {
+    const _base = withLeadingSlash(withTrailingSlash(useRuntimeConfig().app.baseURL))
+    if (_base !== '/' && !props.src.startsWith(_base)) {
+      return joinURL(_base, props.src)
+    }
+  }
+  return props.src
+})
+</script>
 
 <template>
-  <img class="rounded-md w-full">
+  <component
+    :is="ImageComponent"
+    :src="refinedSrc"
+    :alt="alt"
+    :width="width"
+    :height="height"
+    class="rounded-md w-full"
+  />
 </template>
