071e9ec45bc363432df9ac3d36b592af2431baa7
Yu Lia <1938793150@qq.com>
2026-01-09 16:45:18 +0800

    feat: add img compress staged

---
+++ scripts/img-compress-staged.js
@@ -0,0 +1,50 @@
+// @ts-check
+import process from 'node:process'
+import { cancel, confirm, isCancel, spinner } from '@clack/prompts'
+import pico from 'picocolors'
+import Git from 'simple-git'
+import { compressImages } from './img-compress.js'
+
+async function main() {
+  const git = Git()
+  const stagedFiles = (await git.diff(['--cached', '--name-only']))
+    .split('\n')
+    .map(i => i.trim())
+    .filter(Boolean)
+
+  const images = stagedFiles.filter(i => i.match(/\.(png|jpe?g|webp)$/i))
+
+  if (images.length > 0) {
+    console.log('Images to compress:\n', images)
+
+    const shouldCompress = await confirm({
+      message: `Compress ${pico.cyan(images.length)} images in staged area?`,
+      initialValue: true,
+    })
+
+    if (isCancel(shouldCompress) || !shouldCompress) {
+      cancel('Optimization skipped.')
+      process.exit(0)
+    }
+
+    const s = spinner()
+    s.start('Optimizing images...')
+
+    const result = await compressImages(images)
+
+    s.stop('Optimization finished')
+
+    console.log()
+
+    for (const { type, message } of result)
+      type === 'skip' ? console.log(pico.dim(message)) : console.log(pico.green(message))
+
+    console.log()
+  }
+  else {
+    console.log('No images found in staged area')
+    process.exit(0)
+  }
+}
+
+main().catch(console.error)
---
+++ scripts/img-compress.js
@@ -0,0 +1,68 @@
+// @ts-check
+import fs from 'node:fs/promises'
+import sharp from 'sharp'
+
+const MAX_SIZE = 1440
+const SUPPORTED = ['jpeg', 'png', 'webp']
+
+export async function compressWithSharp(image, inBuffer, inFile, outFile) {
+  const { format, width, height } = await image.metadata()
+
+  if (!format || !width || !height)
+    throw new Error(`Could not determine metadata of ${inFile}`)
+
+  if (!SUPPORTED.includes(format))
+    throw new Error(`Unsupported format ${format} of ${inFile}`)
+
+  if (width > MAX_SIZE || height > MAX_SIZE)
+    image = image.resize(MAX_SIZE)
+
+  image = image[format]({
+    quality: format === 'png' ? 100 : 80,
+    compressionLevel: 9,
+  })
+
+  const outBuffer = await image.withMetadata().toBuffer()
+  const size = inBuffer.byteLength
+  const outSize = outBuffer.byteLength
+  const percent = (outSize - size) / size
+
+  return {
+    image,
+    outBuffer,
+    size,
+    outSize,
+    percent,
+    inFile,
+    outFile,
+  }
+}
+
+export async function compressImages(files) {
+  return Promise.all(files.map(async (file) => {
+    const buffer = await fs.readFile(file)
+    const image = sharp(buffer)
+    const { percent, size, outSize, inFile, outFile, outBuffer } = await compressWithSharp(image, buffer, file, file)
+
+    const ratioText = `${(percent * 100).toFixed(1).padStart(6, ' ')}%`
+    const diffText = `${bytesToHuman(size).padStart(7)} -> ${bytesToHuman(outSize).padEnd(7)}`
+
+    const isEfficient = percent <= -0.10
+
+    if (isEfficient)
+      await fs.writeFile(outFile, outBuffer)
+
+    return {
+      type: isEfficient ? 'comp' : 'skip',
+      message: `[${isEfficient ? 'COMP' : 'SKIP'}] ${diffText} ${ratioText}  ${inFile}`,
+    }
+  }))
+}
+
+function bytesToHuman(size) {
+  if (!size)
+    return '0 B'.padStart(10, ' ')
+
+  const i = Math.floor(Math.log(size) / Math.log(1024))
+  return `${(size / 1024 ** i).toFixed(2)} ${['B', 'kB', 'MB', 'GB', 'TB'][i]}`.padStart(10, ' ')
+}
