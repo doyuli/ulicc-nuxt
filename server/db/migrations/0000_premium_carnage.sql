de5a22d2886bfcd3282690b641ef04b66a6fa664
Yu Lia <1938793150@qq.com>
2026-01-29 11:21:11 +0800

    chore: update db migrations

--- server/db/migrations/0000_premium_carnage.sql
+++ server/db/migrations/0000_premium_carnage.sql
@@ -9,11 +9,16 @@ CREATE TABLE "content_summarys" (
 --> statement-breakpoint
 CREATE TABLE "content_vectors" (
 	"id" varchar(191) PRIMARY KEY NOT NULL,
-	"content_id" varchar(191) NOT NULL,
+	"post_id" varchar(191) NOT NULL,
+	"section_id" varchar(191) NOT NULL,
+	"title" text NOT NULL,
+	"heading" text,
 	"content" text NOT NULL,
+	"content_hash" varchar(32) NOT NULL,
 	"embedding" vector(1024) NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "content_vectors_content_id_unique" UNIQUE("content_id")
+	CONSTRAINT "content_vectors_section_id_unique" UNIQUE("section_id")
 );
 --> statement-breakpoint
-CREATE INDEX "embedding_idx" ON "content_vectors" USING hnsw ("embedding" vector_cosine_ops);
+CREATE INDEX "embedding_idx" ON "content_vectors" USING hnsw ("embedding" vector_cosine_ops);--> statement-breakpoint
+CREATE INDEX "post_id_idx" ON "content_vectors" USING btree ("post_id");
